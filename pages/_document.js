20652de00c0e0b7ac7ed746d36c01aeb4d39839e
Johan Steenkamp <johan.steenkamp@gmail.com>
2018-11-09 16:08:46 +1300

    Update packages

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,8 @@
 import Document, { Head, Main, NextScript } from 'next/document';
-import { injectGlobal, ServerStyleSheet } from 'styled-components';
+import { createGlobalStyle, ServerStyleSheet } from 'styled-components';
 
 // CSS reset
-injectGlobal`
+const GlobalStyle = createGlobalStyle`
   body {max-width:839px; margin:auto; font-family:"avenir next", avenir, sans-serif;}
   h1, h2, h3, h4, h5, h6 {margin:0; padding:0; font-family:"avenir next", avenir, sans-serif;}
   * {box-sizing:border-box;}
@@ -11,22 +11,29 @@ injectGlobal`
 `;
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
-    );
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
+
+    const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: [...initialProps.styles, ...sheet.getStyleElement()],
+    };
   }
 
   render() {
     return (
       <html>
         <Head>
-          <title>Orbital - Do the simple thing first</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="shortcut icon" href="/static/favicon.ico"/>
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <GlobalStyle />
           {this.props.styleTags}
         </Head>
         <body>
--- pages/development/index.md
+++ pages/development/index.md
@@ -2,7 +2,7 @@
 
 * [Bret Victor - The Future of Programming](http://worrydream.com/#!/dbx)
 * [Simplicity is Key](https://hvops.com/articles/simplicity-is-key/)
-* [Getting Real [PDF]](./static/getting.real.pdf)
+* [Getting Real [PDF]](./static/archive/getting.real.pdf)
 * [Reconsider](https://m.signalvnoise.com/reconsider-41adf356857f)
 * [README Driven Development](http://tom.preston-werner.com/2010/08/23/readme-driven-development.html)
 * [On being a Senior Engineer](https://www.kitchensoap.com/2012/10/25/on-being-a-senior-engineer/)
